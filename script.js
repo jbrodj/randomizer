3efbe9ef4b755c25294b1f21a8509cc0dd46d5ca
J Brodie Day <jbrodieday@gmail.com>
2022-01-20 16:49:55 -0500

    Make ID names more consistent and clean up javascript logic

--- script.js
+++ script.js
@@ -1,63 +1,53 @@
-const mainForm = document.getElementById('mainForm')
+// Elements from input form for gathering number of players
+const numForm = document.getElementById('numForm')
+// const inputContainer1 = document.getElementById('inputContainer1')
+const numInput = document.getElementById('numInput')
+const numButton = document.getElementById('numSubmit')
 
-const inputContainer = document.getElementsByClassName('inputContainer')
-
-const input1 = document.getElementById('input1')
-const input2 = document.getElementById('input2')
-
-const mainInput = document.getElementById('number')
-
-const mainSubmit = document.getElementsByClassName('mainSubmit')
-
-const mainButton = document.getElementById('mainSubmit')
-
+// Elements from input form for gathering player names
+const nameForm = document.getElementById('nameForm')
+const inputContainer2 = document.getElementById('inputContainer2')
 const nameButton = document.getElementById('nameSubmit')
 
+// Elements for displaying final results
 const resultSection = document.getElementById('resultSection')
 
-const resetButton = document.getElementById('resetButton')
+// const resetButton = document.getElementById('resetButton')
 
-const div1 = document.getElementById('container1')
-const div2 = document.getElementById('container2')
-const div3 = document.getElementById('container3')
-
-// Store existing input value for the number of players text field.
-let numPlayers = parseInt(mainInput.value)
-
-// Declare arrays to store user data
-let numArray = []
-let nameArray = []
+// Variable to store input value for the number of players text field.
+let numPlayers = parseInt(numInput.value)
 
 // Listen for change on number of players input
-mainInput.addEventListener('change', assignNum)
+numInput.addEventListener('change', assignNum)
 // Set value of numPlayers = the form value
 function assignNum(event) {
     numPlayers = parseInt(event.target.value)
 }
 
-// Listen for submit button on main form. 
-mainForm.addEventListener('submit', submitMainForm)
+// Declare arrays to store user data
+let numArray = []
+let nameArray = []
 
-// When submit clicked, send numPlayers value and call next function. 
-function submitMainForm(event) {
+// Listen for submit event on number form. 
+numForm.addEventListener('submit', submitNumForm)
+
+// When submit clicked, send numPlayers value, clean up form elements, and call next function. 
+function submitNumForm(event) {
     event.preventDefault()
-    console.log(numPlayers)
-// Hide first form
-    div1.classList.add('inactive')
-    mainForm.classList.add('inactive')
-    mainButton.classList.add('inactive')
-    div2.classList.remove('inactive')
-    nameButton.classList.remove('inactive')
-// Populate empty array with a number of items = the numPlayers value
+    // Populate empty array with a number of items = the numPlayers value
     numArray = []
     for (let i = 1; i <= numPlayers; i++) {
         numArray.push(`Player${i}`)
     }
-// Call fn to move on to the next form
+    // Hide first form
+    numForm.classList.add('inactive')
+    numButton.classList.add('inactive')
+    nameButton.classList.remove('inactive')
+    // Call fn to display the names form
     generateNamesForm()
 }
 
-// After the initial form submit, we run:
+// After the initial form submit, we generate a form with # inputs === # selected in the first form:
 function generateNamesForm() {
     // Update prompt heading for next step.
     document.querySelector('h2').innerText = `Let's gather all the names`;
@@ -66,7 +56,7 @@ function generateNamesForm() {
         string = element.toString()
         // Create & append div for styling the form containers
         const inputDiv = document.createElement('div')
-        input2.appendChild(inputDiv)
+        inputContainer2.appendChild(inputDiv)
         inputDiv.setAttribute('class', 'inputDiv')
         inputDiv.setAttribute('class', 'altColor')
         // Create label elements and name using numArray to link each to corresponding input element
@@ -74,7 +64,7 @@ function generateNamesForm() {
         label.innerText = string
         label.setAttribute('for', string)
         inputDiv.appendChild(label)
-        // Create & append input elements and 
+        // Create & append input elements and add their properties
         const input = document.createElement('input')
         input.setAttribute('class', 'nameInput')
         input.setAttribute('id', string)
@@ -84,32 +74,28 @@ function generateNamesForm() {
     })
 }
 
-
 nouveauArray = []
 // Listen for second form submit click.
 nameButton.addEventListener('click', submitNameForm)
 // When name form submits, call function & store values. 
 function submitNameForm(event) {
     event.preventDefault()
-        // Update prompt heading for next step.
+    // Update prompt heading for next step.
     document.querySelector('h2').innerText = `Here's the order!`;
+    // Array to store temporary name data
     nameArray = []
+    // Variable to store the input elements in the names form 
     eachName = document.getElementsByClassName('nameInput')
-
+    // Populate array with elements
     nameArray.push(eachName)
-
-    console.log(nameArray)
-    console.log(nameArray[0][0].value)
-
-    console.log(nameArray[0].length)
-
+    // console.log(nameArray)
+    // console.log(nameArray[0][0].value)
+    // console.log(nameArray[0].length)
+    // Loop through the nameArray to populate a new array with the user data from the inputs. 
     for (i = 0; i < nameArray[0].length; i++) {
-        console.log(i)
         nouveauArray.push(nameArray[0][i].value)
     }
-    console.log(nouveauArray)
-
-    // Hide second form.
+    // Hide names input form.
     nameForm.classList.add('inactive')
     nameButton.classList.add('inactive')
 
@@ -118,45 +104,44 @@ function submitNameForm(event) {
 }
 
 // Define final array to store randomized name order.
-const numberizer = []
+const randomOrder = []
 
+// Function to randomize the order of the list of names
 function randomatron2000() {
-    // Generate random number, add item at that index to numberizer array, and remove it from array of names.
+    // Loop to generate random number, add item at that index to randomOrder array, and remove it from array of names for each index in the array.
     while (nouveauArray.length > 0) {
         rando = Math.floor(Math.random() * nouveauArray.length)
-        numberizer.push(nouveauArray[rando])
+        randomOrder.push(nouveauArray[rando])
         nouveauArray.splice(rando, 1)
     }
-
     // Call the print function
     printResults()
 }
 
-// Send final results to the UL on the page, creating an li for each array index.
+// Send final results to the OL on the page, creating an li for each index in the randomized array.
 function printResults() {
+    // Display the results section
     resultSection.classList.remove('inactive')
-
-    numberizer.forEach(function(item) {
+    // Create and append the list items for each index. 
+    randomOrder.forEach(function(item) {
         const listItem = document.createElement('li')
         listItem.classList.add('altColor')
         listItem.innerText = item
         resultSection.append(listItem)
-
     })
 }
 
 // Event Listener for reset button
-resetButton.addEventListener('click', formReset)
+// resetButton.addEventListener('click', formReset)
 
-function formReset() {
-    console.log('run')
-    // if (!mainForm) {
-        console.log('yes')
-        mainForm.classList.remove('inactive')
-        mainSubmit.classList.remove('inactive')
-    // }
-    nameForm.classList.add('inactive')
-    nameButton.classList.add('inactive')
-    resultSection.classList.add('inactive')
-}
-
+// function formReset() {
+//     console.log('run')
+//     // if (!numForm) {
+//         console.log('yes')
+//         numForm.classList.remove('inactive')
+//         numButton.classList.remove('inactive')
+//     // }
+//     .classList.add('inactive')
+//     nameButton.classList.add('inactive')
+//     resultSection.classList.add('inactive')
+// }
